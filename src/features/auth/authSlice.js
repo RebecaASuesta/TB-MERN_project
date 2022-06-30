d6f0221764902ded93c990c0cf14c5ccb85b6504
Rebeca <rebeca.as1994@gmail.com>
2022-06-30 13:00:52 +0200

    store.js actualizado

--- src/features/auth/authSlice.js
+++ src/features/auth/authSlice.js
@@ -4,14 +4,14 @@ const initialState = {
   user: null,
 };
 
+export const register = createAsyncThunk('auth/register',async(user)=>{
+    console.log(user)
+}); 
+
 export const authSlice = createSlice({
   name: "auth",
   initialState,
   reducers: {},
 });
-
-export const register = createAsyncThunk('auth/register',async(user)=>{
-    console.log(user)
-}); 
 
 export default authSlice.reducer
