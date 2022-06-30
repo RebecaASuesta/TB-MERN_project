f292adf6af2789787587708471133853300fe36b
Rebeca <rebeca.as1994@gmail.com>
2022-06-30 18:45:11 +0200

    Componente Post actualizado

--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -1,15 +1,19 @@
+import { Link } from "react-router-dom"
+
 const Post = () => {
     const { posts } = useSelector((state) => state.posts);
 
     const post = posts.map((post) => {
         return (
-            <div className="post">
-                <p>{post.title}</p>
+            <div className="post" key={post.id}>
+                <Link to={"/post/" + post.id}>
+                    <p>{post.title}</p>
+                </Link>
             </div>
         )
     })
 
-    return <div>Post</div>
+    return <div>{post}</div>
 };
 
 export default Post
