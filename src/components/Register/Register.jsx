d739faee6af6e09656806369c967d370a045c702
Rebeca <rebeca.as1994@gmail.com>
2022-06-30 15:32:41 +0200

    isSuccess implementado en Register

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,5 +1,5 @@
-import React, { useState } from 'react'
-import { useDispatch } from 'react-redux'
+import { useEffect, useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import { register } from '../../features/auth/authSlice'
 import { notification } from 'antd'
 
@@ -13,6 +13,17 @@ const Register = () => {
     const { name, email, password, password2 } = formData;
 
     const dispatch = useDispatch();
+
+    const { isSuccess, message } = useSelector((state) => state.auth);
+
+    useEffect(() => {
+        if (isSuccess) {
+            notification.success({
+                message: "Success",
+                description: message
+            })
+        }
+    }, [isSuccess]);
 
     const onChange = (e)=>{
         setFormData((prevState)=> ({
@@ -33,25 +44,25 @@ const Register = () => {
         }
     };
 
-  return (
-    <form onSubmit={onSubmit}>
-        <input type="text" name="name" value={name} onChange={onChange} />
-        <input type="email" name="email" value={email} onChange={onChange}/>
-        <input 
-            type="password"
-            name="password"
-            value={password}
-            onChange={onChange}
-        />
-        <input
-            type="password"
-            name="password2"
-            value={password2}
-            onChange={onChange}
-        />
-        <button type="submit">Register</button>
-    </form>
-  )
+    return (
+        <form onSubmit={onSubmit}>
+            <input type="text" name="name" value={name} onChange={onChange} />
+            <input type="email" name="email" value={email} onChange={onChange}/>
+            <input 
+                type="password"
+                name="password"
+                value={password}
+                onChange={onChange}
+            />
+            <input
+                type="password"
+                name="password2"
+                value={password2}
+                onChange={onChange}
+            />
+            <button type="submit">Register</button>
+        </form>
+    )
 };
 
 export default Register
