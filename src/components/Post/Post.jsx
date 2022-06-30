e60a84fe4113da7cadee8150beccb499b193f914
Rebeca <rebeca.as1994@gmail.com>
2022-06-30 17:43:36 +0200

    Componente Post actualizado

--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -1,7 +1,15 @@
 const Post = () => {
-    return (
-        <div>Post</div>
-    )
-}
-  
+    const { posts } = useSelector((state) => state.posts);
+
+    const post = posts.map((post) => {
+        return (
+            <div className="post">
+                <p>{post.title}</p>
+            </div>
+        )
+    })
+
+    return <div>Post</div>
+};
+
 export default Post
