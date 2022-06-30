4932a3118ce4dcf54150a53a6f676d12b5c81701
Rebeca <rebeca.as1994@gmail.com>
2022-06-30 17:32:47 +0200

    Componente Posts actualizado

--- src/components/Home/Posts/Posts.jsx
+++ src/components/Home/Posts/Posts.jsx
@@ -1,7 +1,14 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
 import Post from '../../Post/Post'
 
 const Posts = () => {
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(getall());
+    }, []);
+    
     return (
         <div>
             <h1>Posts</h1>
