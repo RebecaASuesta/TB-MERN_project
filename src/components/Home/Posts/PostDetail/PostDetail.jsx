0f0305f5368bcfe4a87d398736c41304ae2d059f
Rebeca <rebeca.as1994@gmail.com>
2022-06-30 19:05:15 +0200

    PostDitail actualizado

--- src/components/Home/Posts/PostDetail/PostDetail.jsx
+++ src/components/Home/Posts/PostDetail/PostDetail.jsx
@@ -1,9 +1,27 @@
+import React, { useEffect } from "react"
+import { useSelector } from "react-redux"
+import { useDispatch } from "react-redux"
 import { useParams } from "react-router-dom"
+import { getById } from "../../../../features/posts/postsSlice"
 
 const PostDetail = () => {
     const { id } = useParams();
-    console.log(id);
-    return <div>PostDetail</div>
+
+    const dispatch = useDispatch();
+
+    const { post } = useSelector((state) => state.posts);
+
+    useEffect(() => {
+        dispatch(getById(id))
+    }, []);
+
+    return (
+        <div>
+            <h1>PostDetail</h1>
+            <p>{post.title}</p>
+            <p>{post.body}</p>
+        </div>
+    )
 };
 
 export default PostDetail
