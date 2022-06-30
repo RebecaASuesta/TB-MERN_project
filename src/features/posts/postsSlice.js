f7227ced53a7cdc62072c2bb03cb4c1fd98f708e
Rebeca <rebeca.as1994@gmail.com>
2022-06-30 17:47:17 +0200

    postsSlice actualizado

--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -2,29 +2,38 @@ import { createSlice, createAsyncThunk } from "@reduxjs/toolkit"
 import postsService from "./postsService"
 
 const initialState = {
-    posts: []
+    posts: [],
+    isLoading: false
 };
 
 export const getAll = createAsyncThunk(
     "posts/getAll",
     async () => {
-    try {
-        return await postsService.getAll();
-    } catch (error) {
-        console.error(error);
-    }
-});
+        try {
+            return await postsService.getAll();
+        } catch (error) {
+            console.error(error);
+        }
+    });
 
 export const postsSlice = createSlice({
     name: "posts",
     initialState,
-    reducers: {},
+    reducers: {
+        reset: (state) => {
+            state.isLoading = false;
+        }
+    },
     extraReducers: (builder) => {
-        builder
-            .addCase(getAll.fulfilled, (state, action) => {
-                state.posts = action.payload;
-            })
+        builder.addCase(getAll.fulfilled, (state, action) => {
+            state.posts = action.payload
+        });
+        builder.addCase(getAll.pending, (state) => {
+            state.isLoading = true
+        })
     }
 });
+
+export const { reset } = postsSlice.actions;
 
 export default postsSlice.reducer
