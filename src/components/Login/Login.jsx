978bec3a52b6448ec9b1163748d9e356144c4059
Rebeca <rebeca.as1994@gmail.com>
2022-06-30 15:55:14 +0200

    Componente Login actualizado

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,6 +1,8 @@
-import React, { useState } from 'react'
-import { useDispatch } from 'react-redux'
-import { login } from '../../features/auth/authSlice'
+import { useState, useEffect } from "react"
+import { useDispatch, useSelector } from "react-redux"
+import { useNavigate } from "react-router-dom"
+import { login, reset } from "../../features/auth/authSlice"
+import { notification } from "antd"
 
 const Login = () => {
     const [formData, setFormData] = useState({
@@ -17,7 +19,30 @@ const Login = () => {
         }))
     };
 
+    const navigate = useNavigate();
+  
+    const { isError, isSuccess, message } = useSelector((state) => state.auth);
+
     const dispatch = useDispatch();
+
+    useEffect(() => {
+        if (isError) {
+            notification.error({ 
+                message: "Error",
+                description: message
+            })
+        }
+        if (isSuccess) {
+            notification.success({
+                message: "Success",
+                description: message
+            });
+            setTimeout(() => {
+                navigate("/profile");
+            }, 2000)
+        }
+        dispatch(reset())
+    }, [isError, isSuccess, message]);    
 
     const onSubmit = (e) => {
         e.preventDefault(),
