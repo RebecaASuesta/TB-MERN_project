da55abf7e1c23205251e3782ce3b6b8c78731dc2
Rebeca <rebeca.as1994@gmail.com>
2022-06-30 14:11:27 +0200

    Logout implementado en Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,34 @@
+import { Link, useNavigate } from "react-router-dom"
+import { useDispatch, useSelector } from "react-redux"
+import { logout } from "../../features/auth/authSlice"
+
 const Header = () => {
+    const navigate = useNavigate();
+
+    const dispatch = useDispatch();
+
+    const { user } = useSelector((state) => state.auth);
+
+    const onLogout = (e) => {
+        e.preventDefault();
+        dispatch(logout());
+        navigate("/login")
+    };
+
     return (
-        <div>Header</div>
+        <nav>
+            <span>Header</span>
+            <div>
+                {user ? 
+                    <span><Link to="/" onClick={onLogout}>Logout</Link></span>
+                : 
+                    <>
+                        <span><Link to="/login">Login</Link></span>
+                        <span><Link to="/register">Register</Link></span>
+                    </>
+                }
+            </div>
+        </nav>
     )
 }
   
