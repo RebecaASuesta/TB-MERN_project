c1ad542f0ed2b69bd4c0da7e82f7a591c8870ec4
Rebeca <rebeca.as1994@gmail.com>
2022-06-30 13:29:07 +0200

    Componente Login actualizado con dispatch

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,20 +1,29 @@
 import React, { useState } from 'react'
+import { useDispatch } from 'react-redux'
+import { login } from '../../features/auth/authSlice'
+
 const Login = () => {
     const [formData, setFormData] = useState({
         email:'',
         password:''
-    })
-    const {email,password} = formData
+    });
+
+    const {email,password} = formData;
+
     const onChange = (e)=>{
         setFormData((prevState)=> ({
             ...prevState,
             [e.target.name]:e.target.value,
         }))
-    }
+    };
+
+    const dispatch = useDispatch();
+
     const onSubmit = (e) => {
-        e.preventDefault()
-        console.log('formData',formData)
-    }
+        e.preventDefault(),
+        dispatch(login(formData))
+    };
+
   return (
     <form onSubmit={onSubmit}>
         <input type="email" name="email" value={email} onChange={onChange}/>
@@ -22,5 +31,6 @@ const Login = () => {
         <button type="submit">Login</button>
     </form>
   )
-}
+};
+
 export default Login
